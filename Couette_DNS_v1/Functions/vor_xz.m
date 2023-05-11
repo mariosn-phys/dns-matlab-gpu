ed191763738ca32105c6cd3e7eba4afaa996c030
mariosn-phys <132878795+mariosn-phys@users.noreply.github.com>
2023-05-11 14:49:18 +0300

    Update vor_xz.m

--- Couette_DNS_v1/Functions/vor_xz.m
+++ Couette_DNS_v1/Functions/vor_xz.m
@@ -1,7 +1,7 @@
 function [gx,gz] = vor_xz(u0,v0,w0)
-%x- and z- normal Vorticity fields 
+% x- and z- normal Vorticity fields 
 
-global a b N NX MZ D1x D2x D1z D2z DY D2 DYF D2F yE xE zE dy dx dz A B L 
+% global a b N NX MZ D1x D2x D1z D2z DY D2 DYF D2F yE xE zE dy dx dz A B L 
 
 dydw=difY_F(w0,1);
 dzdv=difZ_F(v0,1);
