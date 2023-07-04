d85f00d3342221535c919e1a26b31bcacd1e84c6
mariosn-phys <132878795+mariosn-phys@users.noreply.github.com>
2023-07-05 01:06:46 +0300

    Update ifft2_cube.m

--- Couette_DNS_v1/Functions/ifft2_cube.m
+++ Couette_DNS_v1/Functions/ifft2_cube.m
@@ -1,5 +1,5 @@
 function [ifft2_single ] = ifft2_cube(spectral_field)
-%2D Inverse Fourier Transorm
+%2D Inverse Fourier Transform
 ifft2_single=real(permute(ifft2(permute(spectral_field,[2 3 1])),[3 1 2]));
 
 end
